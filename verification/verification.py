330947a93582a2af3786583db5eeef3b54bbbaaa
aheimberger <andreasheimberger@gmx.at>
2017-05-05 21:40:32 +0200

    PythonTests: Verification layer some improvements but it's still a bunch of wtf.

--- verification/verification.py
+++ verification/verification.py
@@ -28,11 +28,16 @@ import re
 import filecmp
 import difflib
 
+APPLICATION = "./tests/main"
+APPLICATION_CODE = "./tests/main.cpp"
+FILE_RUN_OUTPUT = "./tests/executed.txt"
+FILE_DEBUG_OUTPUT = "./tests/debugged.txt"
+
 # debug application and save gdb print in file
 exit = False
 def writer(pipe):
     global exit
-    mFile = open("./tests/debugged.txt", "w")
+    mFile = open(FILE_DEBUG_OUTPUT, "w")
 
     for ln in iter(pipe.stdout.readline, ''):
         if (exit == True):
@@ -58,7 +63,7 @@ class Verification:
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        self.mFile = open("./tests/main.cpp", "w")
+        self.mFile = open(APPLICATION_CODE, "w")
         self.mFile.write("#include <string> \n")
         self.mFile.write("#include <iostream> \n")
         self.mFile.write("#define SC_INCLUDE_FX \n")
@@ -69,9 +74,9 @@ class Verification:
         self.mFile.write("      sc_report_handler::set_actions(\"/IEEE_Std_1666/deprecated\", SC_DO_NOTHING); \n\n")
 
     # write main.cpp testcases and call std::cout
-    def createIOStream(self, testcase):
-        self.mTestCases.append(testcase)
-        self.mFile.write("      std::cout << " + testcase +  " << std::endl; \n\n")
+    def addTestCase(self, testvalue):
+        self.mTestCases.append(testvalue)
+        self.mFile.write("      std::cout << " + testvalue + " << std::endl; \n\n")
 
     # closing main.cpp file
     def closeFile(self):
@@ -86,51 +91,63 @@ class Verification:
         INCPATH = os.path.join(SYSTEMC_HOME, "/include")
         LIBPATH = os.path.join(SYSTEMC_HOME, "/lib-linux64")
         SOURCEPATH = os.path.join(SYSTEMC_HOME, "/lib-linux64")
-        os.system("g++ -I. -I" + INCPATH + " -L. -L" + LIBPATH + " -Wl,-rpath=" + SOURCEPATH + " -o ./tests/main ./tests/main.cpp -g -O0 -lsystemc -lm")
+        os.system("g++ -I. -I {0} -L. -L {1} -Wl,-rpath={2} -o {3} {4} -g -O0 -lsystemc -lm".format(INCPATH, LIBPATH, SOURCEPATH, APPLICATION, APPLICATION_CODE))
 
     # execute file and write std::cout
     def executeFile(self):
-        print("Execute File :")
+        print("Execute File:")
         print("-"*90)
-        os.system("./tests/main > ./tests/executed.txt")
+        os.system("{0} > {1}".format(APPLICATION, FILE_RUN_OUTPUT))
+        print("\n\n")
 
     # debug file to get print
     def debugFile(self):
-        print("Debug File :")
+        print("Debug File:")
         print("-"*90)
         sleep = 1
-        num_lines = sum(1 for line in open('./tests/main.cpp'))
+        num_lines = sum(1 for line in open(APPLICATION_CODE))
 
         #fw = open("comp2.txt", "w")
-        pipe = subprocess.Popen(["gdb", "./tests/main", "-q"], stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=False, bufsize=0)
+        pipe = subprocess.Popen(["gdb", APPLICATION, "-q"], stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=False, bufsize=0)
 
         th1 = threading.Thread(target=writer, args=(pipe, ))
         th1.start()
 
         time.sleep(sleep)
-        pipe.stdin.write(bytes("break " + str(num_lines-2) + "\n"))
+        pipe.stdin.write("break {0} \n".format(num_lines-2).encode())
 
         time.sleep(sleep)
-        pipe.stdin.write(bytes("run\n"))
+        pipe.stdin.write("run\n".encode())
 
+        time.sleep(sleep)
+        print("")
+
+        i = 0;
+        maxEntries = len(self.mTestCases)
         for testcase in self.mTestCases:
             time.sleep(sleep)
-            pipe.stdin.write(bytes("print " + testcase + "\n"))
+            pipe.stdin.write("print {0} \n".format(testcase).encode())
+
+            sys.stdout.write('\r')
+            sys.stdout.write("[%-50s] %d%%  " % ('='*int(50/maxEntries*i), int(100/maxEntries*i)))
+            sys.stdout.flush()
+            i += 1
 
         time.sleep(sleep)
-        pipe.stdin.write(bytes("quit\n"))
+        pipe.stdin.write("quit\n".encode())
 
         global exit
         exit = True
         time.sleep(sleep)
+        print("\n\n")
 
     # comparing files to see difference between pretty printer and sdt::cout print
     def compareFiles(self):
-        print("Compare Files :")
+        print("Compare Files:")
         print("-"*90)
 
-        file1 = open("./tests/executed.txt", "r")
-        file2 = open("./tests/debugged.txt", "r")
+        file1 = open(FILE_RUN_OUTPUT, "r")
+        file2 = open(FILE_DEBUG_OUTPUT, "r")
         errorcnt = 0
 
         num_lines1 = sum(1 for line in file1)
@@ -162,48 +179,53 @@ class Verification:
     # ------------------------------------------------------------------------------------------
 
     # create variables
+    def std_string(self, value):
+        varName = self.createVariableName("std_string_")
+        self.mFile.write("      std::string " + varName + " = \"\\n\\n" + value + "\"; \n")
+        self.addTestCase(varName)
+
     def sc_bit(self, value):
         varName = self.createVariableName("sc_bit_")
         self.mFile.write("      sc_bit " + varName + "; \n")
         self.mFile.write("      " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_bv(self, size, value):
         varName = self.createVariableName("sc_bv_")
         self.mFile.write("      sc_bv<" + str(size) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_logic(self, value):
         varName = self.createVariableName("sc_logic_")
         self.mFile.write("      sc_logic " + varName + "; \n")
         self.mFile.write("      " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_lv(self, size, value):
         varName = self.createVariableName("sc_lv_")
         self.mFile.write("      sc_lv<" + str(size) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_int(self, size, value):
         #maxiumum 1 <= size <= 64
         varName = self.createVariableName("sc_int_")
         self.mFile.write("      sc_int<" + str(size) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_uint(self, size, value):
         varName = self.createVariableName("sc_uint_")
         self.mFile.write("      sc_uint<" + str(size) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_bigint(self, size, value):
         varName = self.createVariableName("sc_bigint_")
         self.mFile.write("      sc_bigint<" + str(size) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_biguint(self, size, value):
         varName = self.createVariableName("sc_biguint_")
         self.mFile.write("      sc_biguint<" + str(size) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_fixed(self, size1, size2, value, mod1=None, mod2=None):
         varName = self.createVariableName("sc_fixed_")
@@ -211,7 +233,7 @@ class Verification:
             self.mFile.write("      sc_fixed<" + str(size1) + ", " + str(size2) + ", " + str(mod1) + ", " + str(mod2) + "> " + varName + " = " + value + "; \n")
         else:
             self.mFile.write("      sc_fixed<" + str(size1) + ", " + str(size2) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_ufixed(self, size1, size2, value, mod1=None, mod2=None):
         varName = self.createVariableName("sc_ufixed_")
@@ -219,7 +241,7 @@ class Verification:
             self.mFile.write("      sc_ufixed<" + str(size1) + ", " + str(size2) + ", " + str(mod1) + ", " + str(mod2) + "> " + varName + " = " + value + "; \n")
         else:
             self.mFile.write("      sc_ufixed<" + str(size1) + ", " + str(size2) + "> " + varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_fix(self, size1, size2, value,  mod1=None, mod2=None):
         varName = self.createVariableName("sc_fix_")
@@ -229,7 +251,7 @@ class Verification:
         else:
             self.mFile.write("      sc_fix " + varName + "(" + str(size1) + ", " + str(size2) + ");")
             self.mFile.write(varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
     def sc_ufix(self, size1, size2, value,  mod1=None, mod2=None):
         varName = self.createVariableName("sc_ufix_")
@@ -239,5 +261,5 @@ class Verification:
         else:
             self.mFile.write("      sc_ufix " + varName + "(" + str(size1) + ", " + str(size2) + ");")
             self.mFile.write(varName + " = " + value + "; \n")
-        self.createIOStream(varName)
+        self.addTestCase(varName)
 
