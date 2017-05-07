9ad7e9dbdd5433414f375c7269e7ae5f4476d324
aheimberger <andreasheimberger@gmx.at>
2017-05-07 16:28:37 +0200

    Ci: verification script add pipefail

--- scripts/verification.sh
+++ scripts/verification.sh
@@ -1,4 +1,25 @@
-echo "Project Directory: ${DIR_PROJECT}"
+#!/bin/bash
+
+echo -e "Executing: ${0}"
+set -e
+set -o pipefail
+
+
+# #############################################################################################################
+# variables
+
+FILE_SCRIPT="$(readlink -f $0)"
+DIR_SCRIPTS="$(dirname "${FILE_SCRIPT}")"
+DIR_PROJECT="${DIR_SCRIPTS}/.."
+
+echo "SYSTEMC_VERSION: ${SYSTEMC_VERSION}"
+echo "SYSTEMC_HOME: ${SYSTEMC_HOME}"
+echo "DIR_SCRIPTS: ${DIR_SCRIPTS}"
+echo "DIR_PROJECT: ${DIR_PROJECT}"
+
+
+# #############################################################################################################
+# run
 
 pushd "${DIR_PROJECT}/verification/"
 	python3 main.py
